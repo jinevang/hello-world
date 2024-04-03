3c1e18c3ed3e7a1be2b602a27566791c78ac6eb9
Evan Jin <evan.jin@commandlink.com>
2024-04-03 11:40:51 -0700

    initial

---
+++ src/indexpage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IndexPage = () => {
+  return(
+  <div style={{display: 'flex', flexDirection: 'column', rowGap: '1px'}}>
+    <h1>Hello World</h1>
+    <h4>Hi, I'm Evan. This is for CSS 480.</h4>
+  </div>)
+}
+
+export default IndexPage;
