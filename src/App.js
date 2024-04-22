dfb273a4c83592c819c82495568f806989944f6d
Evan Jin <evan.jin@commandlink.com>
2024-04-21 21:25:27 -0700

    change to hash router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import IndexPage from './pages/indexpage.tsx';
 import NavBar from './components/NavBar.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import ThingsToRead from './pages/thingstoread.tsx';
 
 
 export default function App() {
   return (<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '12px 6px'}}>
-    <BrowserRouter basename='/hello-world'>
+    <HashRouter basename='/hello-world'>
       <NavBar/>
       <Routes>
         <Route path='/' element={<IndexPage/>}/>
         <Route path='/home' element={<IndexPage/>}/>
         <Route path='/things-to-read' element={<ThingsToRead/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </div>
   );
 }
