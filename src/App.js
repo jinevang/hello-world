c8686110492ebe5d95246d12f0d453c257ca6674
Evan Jin <evan.jin@commandlink.com>
2024-04-21 21:42:39 -0700

    add constant paths

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import ThingsToRead from './pages/thingstoread.tsx';
 
 export default function App() {
   return (<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '12px 6px'}}>
-    <HashRouter basename='/hello-world'>
+    <HashRouter>
       <NavBar/>
       <Routes>
         <Route path='/' element={<IndexPage/>}/>
-        <Route path='/home' element={<IndexPage/>}/>
-        <Route path='/things-to-read' element={<ThingsToRead/>}/>
+        <Route path='/hello-world/home' element={<IndexPage/>}/>
+        <Route path='/hello-world/things-to-read' element={<ThingsToRead/>}/>
       </Routes>
     </HashRouter>
     </div>
