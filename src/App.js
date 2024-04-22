63cb2a78778841e6d36f001ffbaf280affe6ae31
Evan Jin <evan.jin@commandlink.com>
2024-04-21 21:57:48 -0700

    update routes?

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import ThingsToRead from './pages/thingstoread.tsx';
 
 export default function App() {
   return (<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '12px 6px'}}>
-    <HashRouter>
       <NavBar/>
       <Routes>
         <Route path='/' element={<IndexPage/>}/>
-        <Route path='/hello-world/home' element={<IndexPage/>}/>
-        <Route path='/hello-world/things-to-read' element={<ThingsToRead/>}/>
+        <Route path='/home' element={<IndexPage/>}/>
+        <Route path='/things-to-read' element={<ThingsToRead/>}/>
       </Routes>
-    </HashRouter>
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,8 @@ import { StyledNavBar } from '../styled/nav-bar.tsx';
 
 const NavBar = () => {
   return(<StyledNavBar>
-    <span><a href='/hello-world/home'>Home</a></span>
-    <span><a href='/hello-world/things-to-read'>Things to Read</a></span>
+    <span><a href='/home'>Home</a></span>
+    <span><a href='/things-to-read'>Things to Read</a></span>
   </StyledNavBar>)
 };
 
